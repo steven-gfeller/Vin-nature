6228ac9cc94ea47d4607c3d024bead977a69c22f
stevengfeller <steven.gfeller@studentfr.ch>
2024-12-16 14:10:40 +0100

    Depuis le début

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,18 @@ const burgerMenu = document.getElementById("burgerMenu");
 const dropdownMenu = document.getElementById("dropdownMenu");
 
 burgerMenu.addEventListener("click", () => {
-  dropdownMenu.style.display =
-    dropdownMenu.style.display === "block" ? "none" : "block";
+  // Toggle display between none and block
+  if (
+    dropdownMenu.style.display === "none" ||
+    dropdownMenu.style.display === ""
+  ) {
+    dropdownMenu.style.display = "flex"; // Show the dropdown menu
+  } else {
+    dropdownMenu.style.display = "none"; // Hide the dropdown menu
+  }
+});
+const closeBtn = document.getElementById("closeBtn");
+
+closeBtn.addEventListener("click", () => {
+  dropdownMenu.style.display = "none"; // Hide the dropdown menu when "X" is clicked
 });
